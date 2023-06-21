1054288191e7837a7c0eeebe8688da039d15ffb3
k2font <shoichiro.kouno@gmail.com>
2023-06-21 17:27:44 +0900

    forループの復習

--- loop.sh
+++ loop.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
+
 a=0
 
 while [ $a -lt 100 ]
 do
   echo $a
   a=`expr $a + 1`
+done
+
+for var in 0 1 2 3 4 5 6 7 8 9
+do
+  echo $var
+done
+
+# ファイルのDirectoryをすべて表示するアプリ
+for FILE in ./*.sh
+do
+  echo $FILE
 done
 
